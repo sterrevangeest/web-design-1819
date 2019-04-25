7ab2204d5868f1c544139e73612cc87e90a072c5
[sterrevangeest] <[sterrevangeest@hotmail.nl]>
2019-04-25 17:01:15 +0200

    update

--- static/index.js
+++ static/index.js
@@ -18,7 +18,7 @@ function wijzigvoorkeuren(event) {
 
 function addtolist(event) {
   console.log(event);
-  if (event.code === "Enter" || event.key === "+") {
+  if (event.type === "click" || event.code === "Enter" || event.key === "+") {
     document.getElementById("activelist").appendChild(event.path[0]);
   }
 }
@@ -26,6 +26,7 @@ function addtolist(event) {
 function removefromlist(event) {
   console.log(event);
   if (
+    event.type === "click" ||
     event.code === "Enter" ||
     event.code === "Backspace" ||
     event.code === "Minus"
@@ -39,10 +40,86 @@ function removefromlist(event) {
 }
 
 function removeallclasses(event) {
-  console.log(event);
-  if (event.code != "Tab") {
+  if (event.code != "Tab" || event.type === "click") {
     document.getElementById("activelist").removeAttribute("id");
     document.getElementById("intactivelist").classList.toggle("inactive");
     document.getElementById("intactivelist").removeAttribute("id");
   }
+}
+
+var content = document.querySelector("body");
+content.onkeydown = function(event) {
+  let values = check(document.activeElement);
+  console.log(values);
+  var id = values[0];
+
+  if (event.keyCode == 40) {
+    console.log("pijl v");
+    var id = Number(id);
+    document.getElementById(id).blur();
+    var nextElement = id + 1;
+    var newId = nextElement.toString();
+    console.log(newId);
+    if (document.getElementById(newId) === null) {
+      document.getElementById(id).focus();
+    } else {
+      console.log(newId);
+      console.log(document.getElementById(newId));
+      document.getElementById(newId).focus();
+    }
+  }
+  if (event.keyCode == 38) {
+    console.log("pijl ^");
+    var id = Number(id);
+    document.getElementById(id).blur();
+    var nextElement = Math.round((id - 1) * 10) / 10;
+    console.log(nextElement);
+    var newId = nextElement.toString();
+    console.log(newId);
+    if (document.getElementById(newId) === null) {
+      document.getElementById(id).focus();
+    } else {
+      console.log(newId);
+      console.log(document.getElementById(newId));
+      document.getElementById(newId).focus();
+    }
+  }
+
+  if (event.keyCode == 39) {
+    console.log("pijl >");
+    var id = Number(id);
+
+    document.getElementById(id).blur();
+    var nextElement = Math.round((id + 0.1) * 10) / 10;
+    console.log(nextElement);
+    var newId = nextElement.toString();
+    console.log(newId);
+
+    if (document.getElementById(newId) === null) {
+      document.getElementById(id).focus();
+    } else {
+      console.log(newId);
+      console.log(document.getElementById(newId));
+      document.getElementById(newId).focus();
+    }
+  }
+  if (event.keyCode == 37) {
+    console.log("pijl <");
+    var id = Number(id);
+    document.getElementById(id).blur();
+    var nextElement = Math.round((id - 0.1) * 10) / 10;
+    var newId = nextElement.toString();
+    if (document.getElementById(newId) === null) {
+      document.getElementById(id).focus();
+    } else {
+      console.log(newId);
+      console.log(document.getElementById(newId));
+      document.getElementById(newId).focus();
+    }
+  }
+};
+
+function check(x) {
+  var id = x.id;
+  return [id];
 }
